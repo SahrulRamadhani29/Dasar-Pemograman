96096824db1fdc807947ec1b28f1402689013d01
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-09-26 07:41:58 +0700

    menambah

---
+++ pemilihanbilangan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class pemilihanbilangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " merupakan Bilangan Genap");
+        } else {
+            System.out.println("Angka " + angka + " merupakan Bilangan Ganjil");
+        }
+    }
+}
