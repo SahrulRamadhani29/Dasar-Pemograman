14637be8f84a284efa04b1707002b6298eedca4d
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-09-26 08:55:00 +0700

    KAFE24

---
+++ Praktikum_05/kafe24.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+/**
+ * kafe24
+ */
+public class kafe24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String menu;
+        char ukuranCup;
+        int jumlah;
+        boolean keanggotaan;
+        System.out.print("Masukan Menu: ");
+        menu = sc.nextLine();
+        System.out.print("Masukan Ukuran Cup: ");
+        ukuranCup = sc.next().charAt(0);
+        System.out.print("Masukan Jumlah: ");
+        jumlah = sc.nextInt();
+        System.out.print("Masukan keanggoraan (true/false): ");
+        keanggotaan =sc.nextBoolean();
+        double hargaMenu = 0;
+        switch (menu.toLowerCase()){
+            case "kopi":
+            hargaMenu = 12000;
+            break;
+            case "teh" :
+            hargaMenu = 7000;
+            break;
+            case "cokolat":
+            hargaMenu = 20000;
+            break;
+        }
+        double totalHarga = hargaMenu * jumlah;
+        switch (ukuranCup) {
+            case 'S':
+            break;                
+            case 'M':
+            totalHarga += 0.25 * totalHarga ;
+            break;
+            case 'L':
+            totalHarga += 0.4* totalHarga;
+            break;
+        }
+        double diskon = keanggotaan ? 0.1 : 0;
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+        
+        System.out.println("Item Pembelian:" + jumlah + " " + menu + "dengan ukuran cup"+ ukuranCup );
+        System.out.println("Nominal Bayar: " + nominalBayar );
+
+    }
+}
