3fa2b806962da393d8ce3c10382e28884b67e0e1
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-09-26 07:39:00 +0700

    Menghapus

---
+++ pemilihanbilternaryoperator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class pemilihanbilternaryoperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " merupakan Bilangan Genap" 
+                                        : "Angka " + angka + " merupakan Bilangan Ganjil";
+        System.out.println(hasil); 
+    }
+}
