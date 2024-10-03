8da9a1c0d75cd0823856d014c2a4294ecb756806
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-10-03 07:50:44 +0700

    menambahkan file baru

---
+++ Praktikum_06/pemilihan2percobaan124.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class pemilihan2percobaan124 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+    int tahun;
+    System.out.print("Masukkan tahun: ");
+    tahun = sc.nextInt();
+    if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+        System.out.println("Tahun Kabisat");
+    } else 
+    System.out.println("Bukan Tahun Kabisat");
+    }
+}
