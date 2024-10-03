f4fbc34022a8123d1ec7dde223b560a99fc1e207
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-10-03 08:10:47 +0700

    Modify without  Logic

---
+++ SoalPemilihan224.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SoalPemilihan224 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = sc.nextInt();
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
