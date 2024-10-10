0b9e7a3b3cf8eb20e3e8feb80a74241b17778f4c
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-10-10 09:13:45 +0700

    pp

--- Praktikum_07/Siakadpercobaan24.java
+++ Praktikum_07/Siakadpercobaan24.java
@@ -7,29 +7,29 @@ int nilai, jml, i=0;
 System.out.print("Masukan jumlah Mahasiswa: ");
 jml = sc.nextInt();
 while (i<jml) {
-    System.out.print("Masukan nilai mahasiswa ke-" + (i+1) + ": ");
+    System.out.print("Masukan nilai mahasiswa ke-" + (i + 1) + ": ");
     nilai = sc.nextInt();
     
     if (nilai < 0 || nilai > 100) {
         System.out.println("Nilai tidak Valid. Masukan lagi nilai yang valid!");
     }
     if (nilai > 80 && nilai <= 100) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah A");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah A");
     }  else if (nilai > 73 && nilai <= 80) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah B+");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah B+");
     }   else if (nilai > 65 && nilai <= 73) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah B");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah B");
     }   else if (nilai > 60 && nilai <= 65) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah C+");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah C+");
     }   else if (nilai > 50 && nilai <= 60) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah C");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah C");
     }   else if (nilai >39 && nilai <= 50) {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah D");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah D");
     } else {
-        System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah E");
+        System.out.println("Nilai mahasiswa ke-" + (i+ 1 ) + "adalah E");
     }
+    i++;
 }
-i++;
 }
 
 }
