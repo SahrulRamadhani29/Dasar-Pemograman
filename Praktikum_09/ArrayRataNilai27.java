590ff6ef13fd36b10707de339efe9cf391499895
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-10-31 08:16:13 +0700

    rata

--- Praktikum_09/ArrayRataNilai27.java
+++ Praktikum_09/ArrayRataNilai27.java
@@ -7,6 +7,7 @@ public class ArrayRataNilai27 {
     int nilaiMhs [] = new int [10];
     double total =0;
     double rata2;
+    int lulus = 0;
 
     for (int i = 0 ; i<nilaiMhs.length; i++) {
         System.out.print("Masukan nilai mahasiswa ke-" + (i+1) + " :");
@@ -14,8 +15,12 @@ public class ArrayRataNilai27 {
     }
     for (int i = 0; i < nilaiMhs.length; i++ ) {
         total += nilaiMhs [i];
+        if (nilaiMhs[i]>70) {
+            lulus ++;
+        }
     }
     rata2 =total/nilaiMhs.length;
     System.out.println("Rata-rata nilai = "+ rata2 );
+    System.out.println("Jumlah Mahasiswa Lulus = " + lulus);
     }
 }
